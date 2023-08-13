05afae32f542c43023374402bb5012e7b2fa60ed
Artem.I <klinok066@gmail.com>
2023-08-13 12:53:26 +0500

    last commit

--- app/src/main/java/com/example/ecocity/MainActivity.java
+++ app/src/main/java/com/example/ecocity/MainActivity.java
@@ -4,13 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.example.ecocity.R.id;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
 
     LinearLayout linearLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,15 +54,35 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.add_activity);
     }
 
-    public void addEventS(View view){
-        RememberState.setType("новости");
-    }
-
-    public void addEventN(View view){
-        RememberState.setType("обращения");
-    }
-
-    public void addEventO(View view){
+    public void addEventS(View view) {
         RememberState.setType("события");
+        setContentView(R.layout.add_sub_s_activity);
+    }
+
+    public void addEventN(View view) {
+        RememberState.setType("новости");
+        setContentView(R.layout.add_sub_n_activity);
+    }
+
+    public void addEventO(View view) {
+        RememberState.setType("обращения");
+        setContentView(R.layout.add_sub_o_activity);
+    }
+
+    public void add_commit(View view) {
+        setContentView(R.layout.add_commit_activity);
+    }
+
+    public void commit_changes(View view) {
+
+        TextInputEditText t1 = (TextInputEditText)findViewById(id.t1);
+        TextInputEditText t2 = (TextInputEditText)findViewById(id.t2);
+
+        RememberState.commit(String.valueOf(t1.getText()), String.valueOf(t2.getText()));
+
+        Toast toast = Toast.makeText(getApplicationContext(), "Запись добавлена", Toast.LENGTH_SHORT);
+        toast.show();
+
+        homePage(view);
     }
 }
--- app/src/main/java/com/example/ecocity/RememberState.java
+++ app/src/main/java/com/example/ecocity/RememberState.java
@@ -6,4 +6,8 @@ public class RememberState {
     public static void setType(String type) {
         RememberState.type = type;
     }
+
+    public static void commit(String title, String text){
+        EventManager.addEvent(new Event(title,type,text,R.drawable.ic_launcher_background));
+    }
 }
